10e2b29bf688c5632bed994b1f54441ddf8c3b4c
James Wright <james.w@xml-int.com>
2023-10-19 13:22:53 +0100

    Add project files.

---
+++ BlazorApp1/Shared/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp1.Shared;
+
+public class Solution
+{
+    public string Path { get; set; }
+    public string Name { get; set; }
+    public string SettingsPath { get; set; }
+    public Status Status { get; set; }
+
+}
+public enum Status
+{ 
+Stopped,
+Loading, Started
+}
+
